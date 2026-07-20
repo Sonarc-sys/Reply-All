fbb2735aadfb4aba4140a65428dd6a4c0f4d50fe
Sonarc-sys <acer213@gmail.com>
2026-07-20 05:33:24 -0400

    Phone Notification Task List Set up

--- script/OfficeUI.gd
+++ script/OfficeUI.gd
@@ -1,49 +1,51 @@
 extends CanvasLayer
 
-const TRIAGE_ITEM_SCENE = preload("res://scene/TriageItem.tscn")
+@export var notification_scene: PackedScene = preload("res://scene/NotificationItem.tscn")
 
-var score_label
-var incident_list
+var score_label: Label
+@onready var notification_list: VBoxContainer = $Phone/ScrollContainer/NotificationList
 
-func _ready():
-	score_label   = get_node("ScoreLabel")
-	incident_list = get_node("TriagePanel/IncidentList")
+# Keep track of active notifications tied to employees
+var active_notifications: Dictionary = {}
 
+func _ready() -> void:
+	print("[OfficeUI] Ready script initialized!")
+	score_label = get_node_or_null("ScoreLabel")
+
+	# Connect signals
 	GameManager.score_changed.connect(update_score)
 	GameManager.incidents_changed.connect(update_triage)
 
+	# Run initial updates
 	update_score(GameManager.score)
 	update_triage()
 
-func update_score(score):
+func update_score(score: int) -> void:
 	if score_label:
 		score_label.text = "Security Score: " + str(score)
 
-func update_triage():
-	if incident_list == null:
-		print("update_triage: incident_list node not found! Check your path.")
-		return
-
-	for child in incident_list.get_children():
-		child.queue_free()
-
+func update_triage() -> void:
+	print("[OfficeUI] update_triage() fired!")
+	
 	for employee in GameManager.employees:
-		if not is_instance_valid(employee):
-			continue
-		if employee.has_issue and employee.current_issue:
-			var item_instance = TRIAGE_ITEM_SCENE.instantiate()
-			incident_list.add_child(item_instance)
+		print("  -> Checking ", employee.employee_name, " | Has Issue: ", employee.has_issue)
+		
+		# Spawning new notifications
+		if employee.has_issue and employee.current_issue != null:
+			if !active_notifications.has(employee):
+				print("  -> Spawning phone notification for ", employee.employee_name)
+				var item = notification_scene.instantiate()
+				notification_list.add_child(item)
+				item.setup(employee)
+				active_notifications[employee] = item
 
-			var item_label = item_instance.find_child("Label", true, false) as Label
-			if item_label:
-				item_label.text = (
-					employee.employee_name
-					+ " ("
-					+ employee.employee_type
-					+ ")\n"
-					+ "⚠️ " + employee.current_issue.employee_message
-					+ "\n\nPatience: "
-					+ str(round(employee.patience))
-					+ "%"
-				)
-				item_label.add_theme_color_override("font_color", Color.WHITE)
+	# Removing notifications for fixed/failed issues
+	var employees_to_remove = []
+	for employee in active_notifications.keys():
+		if !employee.has_issue or employee.current_issue == null:
+			var item = active_notifications[employee]
+			item.queue_free()
+			employees_to_remove.append(employee)
+
+	for emp in employees_to_remove:
+		active_notifications.erase(emp)
